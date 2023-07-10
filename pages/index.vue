c25d07180a8801db0b2136309e7502437debe984
= <dellajannata2112@gmail.com>
2023-07-10 10:34:26 +0700

    Update Tampilan

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,7 @@
                   d="M12,2C7.589,2,4,5.589,4,9.995C3.971,16.44,11.696,21.784,12,22c0,0,8.029-5.56,8-12C20,5.589,16.411,2,12,2z M12,14 c-2.21,0-4-1.79-4-4s1.79-4,4-4s4,1.79,4,4S14.21,14,12,14z" />
               </svg>
 
-              <span class="ml-2">1985 Kerry Way, Whittier, CA, USA</span>
+              <span class="ml-2">Digital Center, Dinas Komunikasi, Kabupaten Mojokerto, Jatim</span>
             </div>
           </li>
           <li class="ml-6">
@@ -81,7 +81,7 @@
     <div class="flex flex-wrap items-center justify-between py-6">
       <div class="w-1/2 md:w-auto">
         <a class='text-white font-bold text-2xl' href='/dentalpro/'>
-          DentalPro
+           Permintaan Surat Permohonan
         </a>
       </div>
 
