c163d83936d909567db27c31111befb3828345a1
DaffaStyn11 <daffasetyan@gmail.com>
2023-07-10 10:11:19 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,26 @@
-<template>
+<!-- <template>
   <h1 class="text-3xl font-bold underline">
     Hello world!
   </h1>
+</template> -->
+<template>
+  <div class="container mx-auto px-4">
+    <h1 class="text-3xl font-bold mb-4">Welcome to My Website</h1>
+    <p class="text-gray-600">This is a template using Tailwind CSS.</p>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-4 rounded">
+      Click Me
+    </button>
+  </div>
 </template>
+
+<script>
+export default {
+  // Komponen Vue
+}
+</script>
+
+<style>
+/* Tidak perlu lagi menggunakan @import untuk tailwind.css */
+
+/* Gaya khusus Anda di sini */
+</style>
