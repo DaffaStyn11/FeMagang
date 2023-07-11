e7ae16ae050c4236fa9ba3a744a7ed15fbbf1d1d
DaffaStyn11 <daffasetyan@gmail.com>
2023-07-11 08:40:30 +0700

    update

--- pages/default.vue
+++ pages/default.vue
@@ -1,12 +1,35 @@
 <template>
-    <div id="">
-        
-        <nuxt>
-            
-        </nuxt>
-    </div>
-<script>
-export default{
-
-}
-</script>
+    <body class="antialiased bg-white font-sans text-gray-900">
+      <main class="w-full">
+        <Header />
+        <Hero />
+        <About />
+        <div class="flex flex-col md:flex-row mt-24 text-center md:-mx-4">
+          <ServiceCard />
+          <ServiceCard />
+          <ServiceCard />
+        </div>
+      </main>
+    </body>
+  </template>
+  
+  <script>
+  import Header from './Header.vue';
+  import Hero from './Hero.vue';
+  import Cta from './Cta.vue';
+  import About from './About.vue';
+  import Footer from './Footer.vue';
+  import Testimonial from './Testimonial.vue';
+  
+  export default {
+    components: {
+      Header,
+      Hero,
+      About,
+      Footer,
+      Cta,
+      Testimonial
+    }
+  }
+  </script>
+  
