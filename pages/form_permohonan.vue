fdbdea3c471e70513becd6f9198a041d2b3a6a8c
Ifa Indrian <ifaindrian152@gmail.com>
2023-07-13 11:20:37 +0700

    update form

--- pages/form_permohonan.vue
+++ pages/form_permohonan.vue
@@ -37,23 +37,15 @@
                             <label for="exampleInputCity1">Id Agenda</label>
                             <input type="text" class="form-control" id="exampleInputCity1" placeholder="Masukkan Id Agenda">
                           </div>
-                          <div class="col-md-6">
-                          <div class="form-group">
-                            <label class="col-sm-3 col-form-label">Status</label>
-                            <div class="col-sm-4">
-                              <div class="form-check">
-                                <label class="form-check-label">
-                                  <input type="radio" class="form-check-input" name="membershipRadios" id="membershipRadios1" value="" checked> Belum </label>
-                              </div>
+                          <!-- <div class="col-md-6"> -->
+                            <div class="form-group">
+                              <label for="exampleSelectGender">Status</label>
+                              <select class="form-control" id="exampleSelectGender">
+                                <option>Belum</option>
+                                <option>Selesai</option>
+                              </select>
                             </div>
-                            <div class="col-sm-5">
-                              <div class="form-check">
-                                <label class="form-check-label">
-                                  <input type="radio" class="form-check-input" name="membershipRadios" id="membershipRadios2" value="option2"> Selesai </label>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
+                        <!-- </div> -->
                           <button type="submit" class="btn btn-primary mr-2">Submit</button>
                           <button class="btn btn-dark">Cancel</button>
                         </form>
